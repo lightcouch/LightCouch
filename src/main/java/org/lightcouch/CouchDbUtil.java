42590cd8ec00bc8cf32fb96274db59aed0d4f8e3
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2011-12-21 16:17:35 +0200

    java.lang.IllegalArgumentException: URI is not hierarchical    from org/lightcouch/CouchDbDesign.java:152:in `getFromDesk' under JRuby

--- src/main/java/org/lightcouch/CouchDbUtil.java
+++ src/main/java/org/lightcouch/CouchDbUtil.java
@@ -40,29 +40,29 @@ final class CouchDbUtil {
 	private CouchDbUtil() {
 		// Utility class
 	}
-	
+
 	public static void assertNotEmpty(Object object, String prefix) throws IllegalArgumentException {
 		if(object == null) {
 			throw new IllegalArgumentException(format("%s may not be null.", prefix));
 		} else if(object instanceof String && ((String)object).length() == 0) {
 			throw new IllegalArgumentException(format("%s may not be empty.", prefix));
-		} 
+		}
 	}
-	
+
 	public static void assertNull(Object object, String prefix) throws IllegalArgumentException {
 		if(object != null) {
 			throw new IllegalArgumentException(format("%s should be null.", prefix));
-		} 
+		}
 	}
-	
+
 	public static String generateUUID() {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
-	
+
 	public static String removeExtension(String fileName) {
 		return fileName.substring(0, fileName.lastIndexOf('.'));
 	}
-	
+
 	// ------------------------------------------------------- JSON
 
 	public static JsonObject objectToJson(Gson gson, Object object) {
@@ -71,7 +71,7 @@ final class CouchDbUtil {
 		}
 		return gson.toJsonTree(object).getAsJsonObject();
 	}
-	
+
 	public static <T> T JsonToObject(Gson gson, JsonElement elem, String key, Class<T> classType) {
 		return gson.fromJson(elem.getAsJsonObject().get(key), classType);
 	}
@@ -80,19 +80,19 @@ final class CouchDbUtil {
 	 * @return A JSON element as a String, or null if not found.
 	 */
 	public static String getElement(JsonObject j, String e) {
-		return (j.get(e) == null) ? null : j.get(e).getAsString();  
+		return (j.get(e) == null) ? null : j.get(e).getAsString();
 	}
-	
+
 	public static long getElementAsLong(JsonObject j, String e) {
 		return (j.get(e) == null) ? 0L : j.get(e).getAsLong();
 	}
-	
+
 	public static int getElementAsInt(JsonObject j, String e) {
 		return (j.get(e) == null) ? 0 : j.get(e).getAsInt();
 	}
-	
+
 	// ----------------------------------------------------- Streams
-	
+
 	private static final String LINE_SEP = System.getProperty("line.separator");
 
 	public static String readFile(File file) {
@@ -100,7 +100,7 @@ final class CouchDbUtil {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(file);
-			while(scanner.hasNextLine()) {        
+			while(scanner.hasNextLine()) {
 				content.append(scanner.nextLine() + LINE_SEP);
 			}
 		} catch (FileNotFoundException e) {
@@ -112,12 +112,12 @@ final class CouchDbUtil {
 	}
 	
 	public static URL getURL(String resource) {
-		return Thread.currentThread().getContextClassLoader().getResource(resource);
+		return CouchDbUtil.class.getResource(resource);
 	}
-	
+
 	  /**
 	 * Closes the response input stream.
-	 * 
+	 *
 	 * @param response The {@link HttpResponse}
 	 */
 	public static void close(HttpResponse response) {
@@ -125,10 +125,10 @@ final class CouchDbUtil {
 			response.getEntity().getContent().close();
 		} catch (Exception e) {}
 	}
-	
+
 	/**
 	 * Closes a resource.
-	 * 
+	 *
 	 * @param c The {@link Closeable} resource.
 	 */
 	public static void close(Closeable c) {
